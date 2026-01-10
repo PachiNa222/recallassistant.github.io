7dbdb5df274e25e5f26b9f3c2f13dff393588c1b
PachiNa <cherryblossoms.ripen@gmail.com>
2026-01-10 16:41:59 +0900

    Update templates.js

--- templates.js
+++ templates.js
@@ -7,35 +7,50 @@
 const TEMPLATES = {
     "math": [
         {
-            name: "数学",
+            name: "図形と方程式",
             items: [
-                { name: "三角関数", relation: "幾何学" },
-                { name: "微分積分", relation: "解析学" },
-                { name: "確率統計", relation: "応用数学" }
+                { name: "内分・外分点", relation: "座標,座標,比→座標" },
+                { name: "円の方程式", relation: "中心,半径→方程式" },
+                { name: "円の方程式", relation: "通る点3つ→方程式" },
+                { name: "直線の方程式", relation: "通る点2つ→方程式" },
+                { name: "直線の方程式", relation: "通る点,傾き→方程式" }
             ]
         },
         {
-            name: "物理",
+            name: "2次方程式",
             items: [
-                { name: "ニュートン力学", relation: "古典" },
-                { name: "相対性理論", relation: "現代" }
+                { name: "解と係数の関係", relation: "2次式→解同士の和と積" },
+                { name: "判別式", relation: "2次式→実数解の個数に関する値" }
             ]
         }
     ],
     "cooking": [
         {
-            name: "和食",
+            name: "水を媒介させる",
             items: [
-                { name: "肉じゃが", relation: "煮物" },
-                { name: "味噌汁", relation: "汁物" }
+                { name: "煮る", relation: "栄養が水に溶け出しやすい" },
+                { name: "蒸す", relation: "蒸し器のセットが大変" }
             ]
         },
         {
-            name: "イタリアン",
+            name: "油を媒介させる",
             items: [
-                { name: "ペペロンチーノ", relation: "パスタ" },
-                { name: "マルゲリータ", relation: "ピザ" }
+                { name: "揚げる", relation: "食材中の水分を飛ばし油と入れ替える" }
             ]
-        }
+        },
+        {
+            name: "金属を媒介させる",
+             items: [
+                { name: "炒める", relation: "焦げないように全体に均等に" },
+                { name: "焼く", relation: "表面に重点的に火を入れる" }
+             ]
+       },
+       {
+            name: "媒介させない",
+             items: [
+                { name: "直火", relation: "焼きムラが出る、危ない" },
+                { name: "炙る", relation: "表面だけを直火でやる" }
+             ]
+       }
     ]
 };
